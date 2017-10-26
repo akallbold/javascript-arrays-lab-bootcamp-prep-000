816903c82aff33462734d1ab8fca223a0ae6a348
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-10-26 17:54:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name){
+  return kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name){
+  return kittens.shift(name);
+}
+
+function appendKitten(name){
+  var newKittens= kittens.push(name);
+  return newKittens;
+}
